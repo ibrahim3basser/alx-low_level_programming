6d9b425bfd843369b0e0a62d2ee38e0e321aba5b
ibrahim3basser <ibrahimelabassery2002@gmail.com>
2023-03-18 11:26:07 -0700

    Good

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-
-	for (int i = 0; i <= 8; i++)
+    int i;
+    int j;
+	for (i = 0; i <= 8; i++)
 	{
-		for (int j = 1; j <= 9; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			if ((i == j) || (j < i))
                 continue;
 
             putchar(i+'0');
             putchar(j+'0');
-            //printf("%d%d, ",i,j);
             if((i!=8) ||(j!=9))
             {
                 putchar(',');
