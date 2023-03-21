281087a957f71586c278fc5f0875223c2305d1a1
ibrahim3basser <ibrahimelabassery2002@gmail.com>
2023-03-21 15:42:35 -0700

    Good

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	putchar('-');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);	
+}
