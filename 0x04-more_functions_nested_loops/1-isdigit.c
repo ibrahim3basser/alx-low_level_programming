a718dbff2fdd476003bcb680c0aecc7355ddace7
ibrahim3basser <ibrahimelabassery2002@gmail.com>
2023-03-24 14:33:05 -0700

    Good

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int x)
+{
+	if (x >= 48 && x <= 57)
+	{
+		return (1);
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+README
+Ibrahim3basser
